bee03d9208b67dc278cd8c5024efca85293b7f46
Aaron Silber <aaron@aaronsilber.me>
2021-02-21 22:56:26 -0500

    Add a script to help enter a shell inside the Nextcloud container

---
+++ shell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# shell.env: get into a shell inside the Nextcloud container
+source .env
+set -eux pipefail
+APP_SERVICE=nextcloud_app
+APP_CONTAINER=$(docker ps -f name=${APP_SERVICE} --quiet --no-trunc)
+SHELL_CMD=/bin/bash
+docker exec \
+         -it \
+         -u ${NEXTCLOUD_UID}:${NEXTCLOUD_GID} \
+         -e "HOME=/config/www/nextcloud" \
+         ${APP_CONTAINER} \
+         /bin/bash \
+         -c "echo \"alias occ='/usr/bin/php /config/www/nextcloud/occ'\" > /config/www/nextcloud/.bashrc; bash"
