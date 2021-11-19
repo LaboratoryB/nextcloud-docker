8928aceae5f442641b06917c5f7343bcffa08aad
Aaron Silber <aaronsilber@users.noreply.github.com>
2021-11-18 20:48:39 -0500

    Update version tags in update.sh script

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 # update.sh - download updates to all containers, and immediately apply them
 set -euxo pipefail
 # pull down the latest container images
-docker pull docker.io/traefik:2.4
+docker pull docker.io/traefik:2.5
 docker pull docker.io/redis:6-alpine
 docker pull ghcr.io/linuxserver/nextcloud
-docker pull docker.io/mariadb:10
+docker pull docker.io/mariadb:10.5
 # redeploy the Swarm service to use the new images
 ./deploy.sh
